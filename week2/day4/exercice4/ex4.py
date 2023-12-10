5b1411c65960f55a245feb88820df43a2b9b20b5
kelly <kellyuzan2905@gmail.com>
2023-12-10 23:16:45 +0200

    relevant message

---
+++ week2/day4/exercice4/ex4.py
@@ -0,0 +1,98 @@
+#exercice 1
+def display_message():
+ 
+ print('css html python')
+
+display_message()
+
+#exercice 2
+def favorite_book (title):
+  print(' One of my favorite books is harry potter')
+
+favorite_book('harry potter')
+
+#exercice 3 
+def describe_city(city,country):
+    print(f'{city} is in {country}')
+
+describe_city('paris','france')
+
+#exercice 4 
+
+import random
+
+def compare_number(user_number):
+    random_number = random.randint(1, 100)
+    if user_number == random_number:
+        print('Succeeded')
+    else:
+        print(f'Your number was {user_number}, but the random number was {random_number}.')
+
+compare_number(29)
+
+#exercice 5 
+def make_shirt(size,text):
+   print ( f the size of the shirt is{size}"and the text is {text}")
+
+  make_shirt ('M,Bonjour')
+
+    #exercice 6
+
+def show_magicians(magicians):
+ for magician in magicians:
+     print(magician)
+    
+def make_shirt(size='large,text'),text=python is fun,')'
+print(f'the size of the shirt is {size} and the text is {text}.')
+make_shirt()
+
+ make_shirt(size='small')
+
+def make_shirt(size='medium',  text='python is fun!'):
+
+#exercice 6
+magician_names = ['Harry Houdini', 'David Blaine', 'Criss Angel']
+def show_magicians(magicians):
+    for magician in magicians:
+        print(magician)
+
+        make_great(magicians):
+         for i in range(len(magicians)):
+      
+         magicians[i]=magicians[i]+'the great'
+         make_great(magician_names)
+         show_magicians(magicians_names)
+         #exercice 7
+
+get_random_temp()
+def get_random_temp(season):
+   if season=='winter':
+
+    return random.uniform(-10,16)
+
+    elif season=='spring'
+
+    return random.uniform(0,23)
+    elif season=='summer':
+    return random.uniform(24,40) 
+    elif season=='autumn':
+    return random.uniform(0,23)
+    else:
+return random.uniform(-10,40)    
+
+ main() 
+ get_random_temp()
+print(f'the temperature right now is{temp:.1f}'degrees celsius.''
+         if temp<0 :
+         print Brrr, that’s freezing! Wear some extra layers today”.)
+         elif 0<==temp =16:
+         print( Quite chilly! Don’t forget your coat”)
+               elif 16<temp<==23:
+               print ('nice and cool,perfect for a day out
+                      .)
+
+         
+
+         
+         
+
